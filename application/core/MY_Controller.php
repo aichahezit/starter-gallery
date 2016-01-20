a45de14868632574622370994f5f35d2f509b929
Aicha Hezit <aichez18@hotmail.com>
2016-01-20 11:13:23 -0800

    Create MY_Controller.php
    case sensitivity

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+ * core/My_Controller.php
+ * 
+ * Default application controller
+ */
+class Application extends CI_Controller {
+    protected $data = array();
+    protected $id;
+    protected $choices = array(
+        'Home' => '/', 'Gallery' => '/gallery', 'About' => '/about'
+    );
+    
+    function __construct() {
+        parent::__construct();
+        $this->data = array();
+        $this->data['pagetitle'] = 'Sample Image Gallery';
+    }
+    
+    function render() {
+        $this->data['menubar'] = build_menu_bar($this->choices);
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+        $this->data['data'] = $this->data;
+        $this->parser->parse('template', $this->data);
+    }
+}
